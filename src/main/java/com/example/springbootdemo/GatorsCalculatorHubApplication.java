35781a8b00bfbc9b9ecda84b6c13371c8f06bf1b
PranavKambhampati <pranav.21950@gmail.com>
2021-02-19 06:49:46 -0800

    Working Gators Website using html/css

---
+++ src/main/java/com/example/springbootdemo/GatorsCalculatorHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatorsCalculatorHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatorsCalculatorHubApplication.class, args);
+    }
+
+}
