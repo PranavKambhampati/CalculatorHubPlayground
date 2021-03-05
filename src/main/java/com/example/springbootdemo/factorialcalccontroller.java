dc99d0b9954bcafcffb5614a25435f730498907e
PranavKambhampati <pranav.21950@gmail.com>
2021-03-05 08:34:33 -0800

    Nothing fixed =(

--- src/main/java/com/example/springbootdemo/factorialcalccontroller.java
+++ src/main/java/com/example/springbootdemo/factorialcalccontroller.java
@@ -1,6 +1,7 @@
 package com.example.springbootdemo;
 
 import com.example.springbootdemo.factorialcalc;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,14 +12,14 @@ import java.io.IOException;
 
 
 @Controller
+@ComponentScan("src/main/resources/static/factorial")
 public class factorialcalccontroller {
 
     @GetMapping("/factorial")
-    public String covid(@RequestParam(name="num", required=true,  defaultValue="10") String num, Model model) throws IOException {
+    public String factorial(@RequestParam(name="num", required=false,  defaultValue="10") String num, Model model) throws IOException {
         int nth = Integer.parseInt(num);
 
         model.addAttribute("num", factorialcalc.driver(nth));
-        return "factorial";
+        return "src/main/resources/static/factorial";
     }
-
 }
